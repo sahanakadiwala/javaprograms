b10a7f8d1b0355997d8131273ed5a40ffca2bd3a
Admin <Admin@LAPTOP-AN0II1ST>
2024-10-09 16:40:57 +0530

    New Codes Added

---
+++ com/javaprograms/p1/Test.java
@@ -0,0 +1,5 @@
+package com.javaprograms.p1;
+
+public class Test {
+
+}
---
+++ com/javaprograms/p1/Testcode1.java
@@ -0,0 +1,10 @@
+package com.javaprograms.p1;
+
+public class Testcode1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
